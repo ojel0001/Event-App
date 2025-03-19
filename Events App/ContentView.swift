b9c6189a8d08c94fdf67b70b902083f9bd042d1a
ojel0001 <ojel0001@algonquinlive.com>
2025-03-19 16:12:32 -0400

    Initial Commit

---
+++ Events App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Events App
+//
+//  Created by sunny ojelabi on 2025-03-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Events App/Events_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Events_AppApp.swift
+//  Events App
+//
+//  Created by sunny ojelabi on 2025-03-19.
+//
+
+import SwiftUI
+
+@main
+struct Events_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
